f4e031aff680be1fd09b84fffadb46b29c98650b
quiclane <developer@veridanlayerseven.com>
2026-02-08 03:09:57 +0200

    spm: use path binaryTarget (repo-root BoringSSL.xcframework)

--- Package.swift
+++ Package.swift
@@ -1,3 +1,3 @@
 // swift-tools-version: 6.2
 import PackageDescription
-let package=Package(name:"boringssl-spm",platforms:[.iOS(.v13)],products:[.library(name:"BoringSSL",targets:["BoringSSL"])],targets:[.binaryTarget(name:"BoringSSL",url:"https://github.com/quiclane/boringssl-spm/releases/download/ios-20260208-305bcfce00b1/BoringSSL.xcframework.zip",checksum:"7b38abcb125c6148280e2aa9379ed0c18f19f554f36e10f5466fff01f2d35d0f")])
+let package=Package(name:"boringssl-spm",platforms:[.iOS(.v13)],products:[.library(name:"BoringSSL",targets:["BoringSSL"])],targets:[.binaryTarget(name:"BoringSSL",path:"BoringSSL.xcframework")])
